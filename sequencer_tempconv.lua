41bb0a2e4d95fb32f4c052167d797f66c7da070a
Won Lee <won_lee@g.harvard.edu>
2016-08-07 19:21:04 -0400

    batch sequencer

--- sequencer_tempconv.lua
+++ sequencer_tempconv.lua
@@ -5,10 +5,10 @@ require 'rnn';
 cmd = torch.CmdLine()
 
 -- Cmd Args
-cmd:option('-datafile', 'checkpoint_seq/lstm_states.h5', 'data file')
-cmd:option('-tagfile', 'convert_seq/data.hdf5', 'tag file for training')
-cmd:option('-testfile', 'checkpoint_seq/lstm_states_test.h5', 'raw words for test')
-cmd:option('-testtagfile', 'convert_seq/data_test.hdf5', 'tag file for test')
+cmd:option('-datafile', 'checkpoint_seq/lstm_states_pad.h5', 'data file')
+cmd:option('-tagfile', 'convert_seq/data_pad.hdf5', 'tag file for training')
+cmd:option('-testfile', 'checkpoint_seq/lstm_states_pad_test.h5', 'raw words for test')
+cmd:option('-testtagfile', 'convert_seq/data_pad_test.hdf5', 'tag file for test')
 cmd:option('-savefile', 'checkpoint_seq/tempconv', 'output file for checkpoints')
 cmd:option('-testoutfile', 'seq_test_results.hdf5', 'output file for test')
 cmd:option('-gpu', 0, 'whether to use gpu')
