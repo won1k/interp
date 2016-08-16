365f8216109cf4df3a4b7a6d23f7c7288c0a81ab
Won Lee <won_lee@g.harvard.edu>
2016-08-16 15:11:38 -0400

    autoencoder sequencer

--- sequencer_enc.lua
+++ sequencer_enc.lua
@@ -120,15 +120,13 @@ function train(data, valid_data, model, criterion)
            -- Decoder forward prop
            forwardConnect(encoder, decoder)
            local decoderInput = { input[{{sentlen + 1}, { batch_idx + 1, batch_idx + batch_size }}] }
-           local decoderOutput = { decoder:forward(decoderInput[1])[1] }
+           local decoderOutput = { decoder:forward(decoderInput[1])[1]:reshape(1, batch_size, data.nfeatures) }
            for t = 2, output_mb:size(1) do
              local _, nextInput = decoderOutput[t-1]:max(2)
              table.insert(decoderInput, nextInput:reshape(1,batch_size):double())
              table.insert(decoderOutput, decoder:forward(decoderInput[t])[1]:reshape(1, batch_size, data.nfeatures))
            end
            decoderInput = nn.JoinTable(1):forward(decoderInput)
-           print(decoderOutput)
-           print(output_mb:size())
            decoderOutput = nn.JoinTable(1):forward(decoderOutput)
            -- Decoder backward prop
            trainErr = trainErr + criterion:forward(decoderOutput, output_mb)
