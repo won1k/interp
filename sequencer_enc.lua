21096453fd09cf0d7d42e915d5798ff33b60ecb2
Won Lee <won_lee@g.harvard.edu>
2016-08-16 15:10:49 -0400

    autoencoder sequencer

--- sequencer_enc.lua
+++ sequencer_enc.lua
@@ -124,9 +124,11 @@ function train(data, valid_data, model, criterion)
            for t = 2, output_mb:size(1) do
              local _, nextInput = decoderOutput[t-1]:max(2)
              table.insert(decoderInput, nextInput:reshape(1,batch_size):double())
-             table.insert(decoderOutput, decoder:forward(decoderInput[t])[1]:view(1, batch_size, data.nfeatures))
+             table.insert(decoderOutput, decoder:forward(decoderInput[t])[1]:reshape(1, batch_size, data.nfeatures))
            end
            decoderInput = nn.JoinTable(1):forward(decoderInput)
+           print(decoderOutput)
+           print(output_mb:size())
            decoderOutput = nn.JoinTable(1):forward(decoderOutput)
            -- Decoder backward prop
            trainErr = trainErr + criterion:forward(decoderOutput, output_mb)
