30d3509c36c0c44c9577465fc190df9341af3ba3
Won Lee <won_lee@g.harvard.edu>
2016-08-24 17:05:36 -0400

    aug tempconv

--- sequencer_tempconv_aug.lua
+++ sequencer_tempconv_aug.lua
@@ -7,10 +7,10 @@ cmd = torch.CmdLine()
 -- Files/settings
 cmd:option('-datafile','convert_seq/data.hdf5','data directory. Should contain data.hdf5 with input data')
 cmd:option('-testfile','convert_seq/data_test.hdf5','data directory. Should contain data.hdf5 with input data')
-cmd:option('-savefile', 'checkpoint_seq/lm','filename to autosave the checkpoint to')
+cmd:option('-savefile', 'checkpoint_seq/lm_aug','filename to autosave the checkpoint to')
 cmd:option('-testoutfile', 'lm_results.hdf5', 'filename to save results to')
 cmd:option('-gpu', 1, 'which gpu to use. -1 = use CPU')
-cmd:option('-feature', 'chunk', 'which feature to use (chunk/pos)')
+cmd:option('-feature', 'chunk', 'which feature to use (chunk/pos/none)')
 -- Hyperparameters
 cmd:option('-dword', 50, 'dimensionality of word embeddings')
 cmd:option('-dfeature', 5, 'dimensionality of feature embeddings')
@@ -30,22 +30,26 @@ function data:__init(data_file)
    self.input_word = {}
    self.input_feature = {}
    self.output = {}
-
    self.lengths = f:read('sent_lens'):all()
    self.max_len = f:read('max_len'):all()[1]
    self.nfeatures_word = f:read('nfeatures'):all():long()[1]
-   self.nfeatures_feature = f:read('nclasses_' .. opt.feature):all():long()[1]
+   if opt.feature ~= 'none' then
+     self.nfeatures_feature = f:read('nclasses_' .. opt.feature):all():long()[1]
+   end
    self.length = self.lengths:size(1)
    self.dwin = f:read('dwin'):all():long()[1]
-
    for i = 1, self.length do
      local len = self.lengths[i]
      self.input_word[len] = f:read(tostring(len)):all():double()
-     self.input_feature[len] = f:read(tostring(len) .. opt.feature):all():double()
+     if opt.feature ~= 'none' then
+       self.input_feature[len] = f:read(tostring(len) .. opt.feature):all():double()
+     end
      self.output[len] = f:read(tostring(len) .. "output"):all():double()
      if opt.gpu > 0 then
        self.input_word[len] = self.input_word[len]:cuda()
-       self.input_feature[len] = self.input_feature[len]:cuda()
+       if opt.feature ~= 'none' then
+         self.input_feature[len] = self.input_feature[len]:cuda()
+       end
        self.output[len] = self.output[len]:cuda()
      end
    end
@@ -61,22 +65,28 @@ function data.__index(self, idx)
    if type(idx) == "string" then
       return data[idx]
    else
-      input_word = self.input_feature[idx]
-      input_feature = self.input_feature[idx]
-      output = self.output[idx]
+     if opt.feature ~= 'none' then
+       return {self.input_word[idx], self.input_feature[idx], self.output[idx]}
+     else
+       return {self.input_word[idx], self.output[idx]}
+     end
    end
-   return {input_word, input_feature, output}
 end
 
 function make_model(train_data)
   local model = nn.Sequential() -- input: {sentlen x batch_size, sentlen x batch_size}
   local wordLT = nn.LookupTable(train_data.nfeatures_word, opt.dword) -- sentlen x batch_size x dim
-  local featureLT = nn.LookupTable(train_data.nfeatures_feature, opt.dfeature)
-  model:add(nn.ParallelTable()
-    :add(wordLT)
-    :add(featureLT)
-  ) -- {sentlen x batch_size x dword, sentlen x batch_size x dfeature}
-  model:add(nn.JoinTable(3)) -- sentlen x batch_size x (dword + dfeature)
+  local featureLT
+  if opt.feature ~= 'none' then
+    featureLT = nn.LookupTable(train_data.nfeatures_feature, opt.dfeature)
+    model:add(nn.ParallelTable()
+      :add(wordLT)
+      :add(featureLT)
+    ) -- {sentlen x batch_size x dword, sentlen x batch_size x dfeature}
+    model:add(nn.JoinTable(3)) -- sentlen x batch_size x (dword + dfeature)
+  else
+    model:add(wordLT) -- sentlen x batch_size x dword
+  end
   model:add(nn.SplitTable(1,3)) -- sentlen table of batch_size x (dword + dfeature)
   local nnlm = nn.Sequential()
   nnlm:add(nn.Linear(opt.dword + opt.dfeature, opt.dhid)) -- sentlen table of batch_size x dhid
@@ -111,10 +121,16 @@ function train(train_data, test_data, model, criterion)
       for sent_idx = 1, torch.ceil(nsent / opt.bsize) do
         local batch_idx = (sent_idx - 1) * opt.bsize
         local batch_size = math.min(sent_idx * opt.bsize, nsent) - batch_idx
-        local input_word_mb = d[1][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
-        local input_feature_mb = d[2][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
-        local input_mb = {input_word_mb, input_feature_mb}
-        local output_mb = d[3][{{ batch_idx + 1, batch_idx + batch_size }}]:reshape(batch_size, sentlen)
+        local input_mb, output_mb
+        if opt.feature ~= 'none' then
+          local input_word_mb = d[1][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
+          local input_feature_mb = d[2][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
+          input_mb = {input_word_mb, input_feature_mb}
+          output_mb = d[3][{{ batch_idx + 1, batch_idx + batch_size }}]:reshape(batch_size, sentlen)
+        else
+          input_mb = d[1][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
+          output_mb = d[2][{{ batch_idx + 1, batch_idx + batch_size }}]:reshape(batch_size, sentlen)
+        end
         output_mb = nn.SplitTable(2):forward(output_mb)
 
         criterion:forward(model:forward(input_mb), output_mb)
@@ -151,9 +167,16 @@ function eval(data, model, criterion)
     for sent_idx = 1, torch.ceil(nsent / opt.bsize) do
       local batch_idx = (sent_idx - 1) * opt.bsize
       local batch_size = math.min(sent_idx * opt.bsize, nsent) - batch_idx
-      local input_word_mb = d[1][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
-      local input_feature_mb = d[2][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
-      local output_mb = d[3][{{ batch_idx + 1, batch_idx + batch_size }}]:reshape(batch_size, sentlen)
+      local input_mb, output_mb
+      if opt.feature ~= 'none' then
+        local input_word_mb = d[1][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
+        local input_feature_mb = d[2][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
+        input_mb = {input_word_mb, input_feature_mb}
+        output_mb = d[3][{{ batch_idx + 1, batch_idx + batch_size }}]:reshape(batch_size, sentlen)
+      else
+        input_mb = d[1][{{ batch_idx + 1, batch_idx + batch_size }}]:transpose(1,2)
+        output_mb = d[2][{{ batch_idx + 1, batch_idx + batch_size }}]:reshape(batch_size, sentlen)
+      end
       output_mb = nn.SplitTable(2):forward(output_mb)
 
       nll = nll + criterion:forward(model:forward({input_word_mb, input_feature_mb}), output_mb) * batch_size
@@ -166,32 +189,37 @@ end
 
 function predict(data, model)
   model:evaluate()
-  local output = hdf5.open(opt.testoutfile, 'w')
+  local results = hdf5.open(opt.testoutfile, 'w')
   local accuracy = 0
   local total = 0
   for i = 1, data.length do
     local sentlen = data.lengths[i]
     local d = data[sentlen]
     local nsent = d[1]:size(1)
-    local input_word = d[1]:transpose(1,2)
-    local input_feature = d[2]:transpose(1,2)
-    local output = d[3]:reshape(nsent, sentlen)
-    local test_pred = model:forward({input_word, input_feature})
+    local input, output
+    if opt.feature ~= 'none' then
+      local input_word = d[1]:transpose(1,2)
+      local input_feature = d[2]:transpose(1,2)
+      input = {input_word, input_feature}
+      output = d[3]:reshape(batch_size, sentlen)
+    else
+      input = d[1]:transpose(1,2)
+      output = d[2]:reshape(batch_size, sentlen)
+    end
+    local test_pred = model:forward(input)
     local maxidx = {}
     for j = 1, #test_pred do
       _, maxidx[j] = test_pred[j]:max(2)
     end
     maxidx = nn.JoinTable(2):forward(maxidx)
-    print(maxidx:size())
-    output:write(tostring(sentlen), maxidx:long())
-    output:write(tostring(sentlen) .. '_target', test_output:long())
-    accuracy = accuracy + torch.eq(maxidx:long(), test_output:long()):sum()
-    total = total + test_output:long():ge(0):sum()
+    results:write(tostring(sentlen), maxidx:long())
+    results:write(tostring(sentlen) .. '_target', output:long())
+    accuracy = accuracy + torch.eq(maxidx:long(), output:long()):sum()
+    total = total + output:long():ge(0):sum()
   end
-  output:write('dwin', torch.Tensor{opt.dwin}:long())
   accuracy = accuracy / total
-  output:write('accuracy', torch.Tensor{accuracy}:double())
-  output:close()
+  results:write('accuracy', torch.Tensor{accuracy}:double())
+  results:close()
   print('Accuracy', accuracy)
 end
 
@@ -201,6 +229,9 @@ function main()
     if opt.gpu > 0 then
       require 'cutorch';
       require 'cunn';
+    end
+    if opt.feature == 'none' then
+      opt.dfeature = 0
     end
 
     -- Load training data
