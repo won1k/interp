94d1bba111187abab610de4d7d5f26d7309430e2
W Ryan Lee <ryanwonlee@gmail.com>
2016-09-21 15:27:31 -0400

    check results

--- enc_sample_post.py
+++ enc_sample_post.py
@@ -9,6 +9,7 @@ with open('convert_seq/ptb_seq.dict','r') as f:
 	for row in f:
 		row = row[0].split(' ')
 		idx2w[int(row[1])] = row[0]
+	print "Dict loaded!"
 
 # Load/translate results
 with open('enc_ptb_results_train_words.txt','w') as f:
@@ -20,13 +21,15 @@ with open('enc_ptb_results_train_words.txt','w') as f:
 			sentence = ' '.join(sentence)
 			f.write(sentence + '\n')
 	train.close()
+	print "Train file translated!"
 
 with open('enc_ptb_results_valid_words.txt','w') as f:
 	test = h5py.File('enc_ptb_results_valid.hdf5', 'r')
 	for key in test.keys():
-		indices = np.transpose(train[key])
+		indices = np.transpose(test[key])
 		for row in indices:
 			sentence = [idx2w[idx] for idx in row]
 			sentence = ' '.join(sentence)
 			f.write(sentence + '\n')
 	test.close()
+	print "Valid file translated!"
