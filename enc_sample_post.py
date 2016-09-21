18f4c1f325ab80a0eb3f454fd4096076e96f7a7b
W Ryan Lee <ryanwonlee@gmail.com>
2016-09-21 15:23:22 -0400

    check results

--- enc_sample_post.py
+++ enc_sample_post.py
@@ -1,4 +1,31 @@
 import h5py
+import numpy as np
 
-# Load results
-f = h5py.File('enc_ptb_results_train.hdf5', 'r')
+# Load dict
+idx2w = {}
+with open('convert_seq/ptb_seq.dict','r') as f:
+	f = csv.reader(f)
+	for row in f:
+		row = row[0].split(' ')
+		idx2w[int(row[1])] = row[0]
+
+# Load/translate results
+with open('enc_ptb_results_train_words.txt','w') as f:
+	train = h5py.File('enc_ptb_results_train.hdf5', 'r')
+	for key in train.keys():
+		indices = np.transpose(train[key])
+		for row in indices:
+			sentence = [idx2w[idx] for idx in row]
+			sentence = ' '.join(sentence)
+			f.write(sentence + '\n')
+	train.close()
+
+with open('enc_ptb_results_valid_words.txt','w') as f:
+	test = h5py.File('enc_ptb_results_valid.hdf5', 'r')
+	for key in test.keys():
+		indices = np.transpose(train[key])
+		for row in indices:
+			sentence = [idx2w[idx] for idx in row]
+			sentence = ' '.join(sentence)
+			f.write(sentence + '\n')
+	test.close()
